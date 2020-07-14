e5a245d4127b4d6ab4cd9617f6f63cbe7328f7e7
LuisCortes98 <luism.cortesj@gmail.com>
2020-07-13 22:18:56 -0500

    Version_Final2

--- test.py
+++ test.py
@@ -8,41 +8,40 @@ dato= ""
 print("Este programa le permite consultar la poblacion por ciudades segun el nivel que se consulte, si se busca un pais, se debería mostrara sus estados y cada estado sus ciudades con su respectiva poblacion; sí se consulta un estado se mostrara el estado con todas sus ciudades y su respectiva poblacion; y por ultimo si se consulta una ciudad se mostrara su poblacion. \n")
 
 while 1:
-    inp = input("Escriba 1 si desea consultar por pais, 2 si desea consultar por estado y 3 si desea consultar por ciudad \n")
+    inp = input("Escriba 1 si desea consultar por pais, 2 si desea consultar por estado y 3 si desea consultar por ciudad: \n")
 
     if inp == 1:
-        dato = raw_input("Ingrese el pais ")
-        Cursor.execute("SELECT Name_Country From [Test].[dbo].[Countries] where Name_Country = (?)", dato)
+        dato = raw_input("Ingrese el pais: ")
+        Cursor.execute("SELECT [Name_Country], [Name_State], [Name_City], [Population_City] FROM [Test_Poblacion].[dbo].[Population_City] WHERE Name_Country = (?) order by Id_country, Id_state, Id_City", dato)
         Table = Cursor.fetchall()
         if Table != None:
-            Cursor.execute("SELECT a.Name_Country, b.Name_State, c.Name_City, c.Population_City FROM [Test].[dbo].[Countries] as a left join [Test].[dbo].[States] as b ON a.Id_Country = b.Id_Country left join [Test].[dbo].[Cities] as c ON b.Id_State = c.Id_State WHERE a.Name_Country = (?) ORDER BY b.Id_State desc", dato)
-            Table = Cursor.fetchall()
-            print (Table)
+            for item in Table:
+                print ("Pais:" + item[0] + " | " + "Estado:" + item[1] + " | " + "Ciudad:" + item[2] + " | " + "Poblacion:" + item[3])
+                print ("\n")
         else:
             print ("No se encuentra el pais")
         break
 
     if inp == 2:
-        dato = raw_input("Ingrese el estado ")
-        Cursor.execute("SELECT Name_State From [Test].[dbo].[States] where Name_State = (?)", dato)
+        dato = raw_input("Ingrese el estado: ")
+        Cursor.execute("SELECT [Name_Country], [Name_State], [Name_City], [Population_City] FROM [Test_Poblacion].[dbo].[Population_City] WHERE Name_State = (?) order by Id_country, Id_state, Id_City", dato)
         Table = Cursor.fetchall()
         if Table != None:
-            Cursor.execute("SELECT Name_State, Name_City, Population_City FROM [Test].[dbo].[States] as a left join [Test].[dbo].[Cities] as b ON a.Id_State = b.Id_State WHERE Name_State = (?) ORDER BY a.Id_State DESC", dato)
-            Table = Cursor.fetchall()
-            print (Table)
-        else: 
+            for item in Table:
+                print ("Pais:" + item[0] + " | " + "Estado:" + item[1] + " | " + "Ciudad:" + item[2] + " | " + "Poblacion:" + item[3])
+                print ("\n")
+        else:
             print ("No se encuentra el estado")
-        
         break
 
     if inp == 3:
-        dato = raw_input("Ingrese la ciudad ")
-        Cursor.execute("SELECT Name_City From [Test].[dbo].[Cities] where Name_City = (?)", dato)
+        dato = raw_input("Ingrese la ciudad: ")
+        Cursor.execute("SELECT [Name_Country], [Name_State], [Name_City], [Population_City] FROM [Test_Poblacion].[dbo].[Population_City] WHERE Name_City = (?) order by Id_country, Id_state, Id_City", dato)
         Table = Cursor.fetchall()
         if Table != None:
-            Cursor.execute("SELECT Name_City, Population_City FROM [Test].[dbo].[Cities] WHERE Name_City = (?)", dato)
-            Table = Cursor.fetchall()
-            print (Table)
+            for item in Table:
+                print ("Pais:" + item[0] + " | " + "Estado:" + item[1] + " | " + "Ciudad:" + item[2] + " | " + "Poblacion:" + item[3])
+                print ("\n")
         else:
             print ("No se encuentra la ciudad")
         break
